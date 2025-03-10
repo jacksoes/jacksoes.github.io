bdb7198a1ad8ac2a7f31f7a3d55659bc4e5ac196
jack soesman <jsoesman1@yahoo.com>
2025-03-10 18:49:58 -0400

    change server ip

--- app.js
+++ app.js
@@ -4,29 +4,77 @@ function testForm() {
     let testi = document.querySelector("#test").value;
     console.log(testi)
 
-    test(testi)   
-  
+    test(testi)
+
 }
 
 function test(value) {
-  
+
     fetch(serverURI, {
         method: "POST",
         body: JSON.stringify({
-          promptRating: `give me an exact rating out of 5 of the general difficulty of the class ${value} return only a length of 3 in the format $.$, use factors such as fail rate and course level,  if you do not recognize the course return the string: 'invalid'`,
-          promptSubject: `give me a list of subjects that will be learned in the class: ${value} format it as a javscript array`
+            promptRating: `give me an exact rating out of 5 of the general difficulty of the class ${value} return only a length of 3 in the format $.$, use factors such as fail rate and course level,  if you do not recognize the course return the string: 'invalid'`,
+            promptSubject: `give me a list of subjects that will be learned in the class: ${value} format it as a javscript array like so [$, $, $, $] the $ are strings of topics typically covered.`,
+            promptResource: `give me a list of resources that will be useful in the class: ${value} format it as a javscript array like so [$, $, $, $] the $ are strings of resources such as websites or books when taking the specified class.`,
         }),
         headers: {
-          "Content-type": "application/json; charset=UTF-8",
+            "Content-type": "application/json; charset=UTF-8",
         }
-      })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-   
+    })
+        .then((response) => response.text())
+        .then((prompts) => {
+            let promptsArr = prompts.split("&split")
+            console.log(promptsArr)
+
+            promptsArr[1] = promptsArr[1].substring(2, promptsArr[1].length - 1)
+            promptsArr[2] = promptsArr[2].substring(2, promptsArr[2].length - 1)
+            console.log(promptsArr)
+
+            promptsArr[1] = promptsArr[1].split(',')
+            promptsArr[2] = promptsArr[2].split(',')
+
+            let topics = document.createElement("ul");
+            let resources = document.createElement("ul");
+
+            let oldTopics = document.querySelector("#topics-list")
+            while (oldTopics.firstChild) {
+                oldTopics.removeChild(oldTopics.firstChild);
+              }
+
+            promptsArr[1].forEach(topic => {
+                let newTopic = document.createElement("li")
+                newTopic.innerHTML = topic
+                oldTopics.appendChild(newTopic)
+
+            });
+
+          
+            let oldResources = document.querySelector("#resources-list")
+
+            while (oldResources.firstChild) {
+                oldResources.removeChild(oldResources.firstChild);
+              }
+            promptsArr[2].forEach(resource => {
+                let newResource = document.createElement("li")
+                newResource.innerHTML = resource
+                oldResources.appendChild(newResource)
+            })
+
+
+            let oldRating = document.querySelector("#test-rating")
+            let oldName = document.querySelector("#test-name")
+            let totalRating = "difficulty: " + promptsArr[0] + "/5"
+            oldRating.innerHTML = totalRating
+            oldName.innerHTML = value
+        });
+
+
+
+
 
 }
 
-function testin(){
+function testin() {
     test();
 }
 
--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = 3000;
 app.use(cors());
 
 const genAI = new GoogleGenerativeAI("AIzaSyD0mlSqRX03sIdjhcY3qTWlob4FibasYas");
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-pro-exp-02-05" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 //const prompt = "Explain how AI works";
 
@@ -31,13 +31,11 @@ app.post("/", async function(req,res){
 
     const prompts = JSON.parse(JSON.stringify(req.body))
     console.log(prompts.promptRating);
-    const result = await model.generateContent(prompts.promptRating)
+    const result = await model.generateContent(prompts.promptRating +"\n" + prompts.promptSubject + "\n" + prompts.promptResource + "\nSeparate each of the 3 responses with the string '&split'")
     console.log(result.response.text());
 
-    res.send({title : result.response.text()})
+    res.send(result.response.text())
     
 })
 
-app.listen(port, () => {
-    console.log("server running at" + port)
-})
+app.listen(port, "0.0.0.0")
