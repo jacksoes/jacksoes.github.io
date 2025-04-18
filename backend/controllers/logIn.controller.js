c687a690d23bfb4f7404a6266988e9e73853c9f2
jack soesman <jsoesman1@yahoo.com>
2025-04-18 18:52:33 -0400

    add encryption to password on sign up

--- backend/controllers/logIn.controller.js
+++ backend/controllers/logIn.controller.js
@@ -5,6 +5,18 @@ const logInController = (req, res) => {
     console.log("login controller ran")
 
 
+    const testUser = {
+            _id: "testid",
+            userName: "testuser",
+            password: "testpassword",
+        }
+    
+    const testingUser = new User(testUser);
+
+
+    //if user found log in AND password matches
+   // await User.findById(testUser.testid) ?  :
+
 }
 
 module.exports = logInController;
--- backend/controllers/signUp.controller.js
+++ backend/controllers/signUp.controller.js
@@ -1,39 +1,56 @@
-const User = require("../models/users.models.js")
+const User = require("../models/users.models.js");
+const bcrypt = require("bcrypt");
 
-const signUpController = async(req, res) => {
-    console.log("sign up controller ran")
+const signUpController = async (req, res) => {
+  console.log("sign up controller ran");
 
-    
+  const testUser = {
+    _id: "testid",
+    userName: "testuser",
+    password: "testpassword",
+  };
 
-    const testUser = {
-        _id: "testid",
-        userName: "testuser",
-        password: "testpassword",
-    }
+  const testingUser = new User(testUser);
 
-    const testingUser = new User(testUser);
-    //encrypt password
-    
-    
-    // refine user as json
+  if(await User.findById(testUser.testid)){
+    res.send({ message: "User already exists" });
+  }
 
-    //check if user exists by id
-    //
+ 
+  const saltRounds = 10;
 
-    await User.findById(testUser.testid) ? async () => {await User.create(testingUser); res.send({message: "User is successfully signed up."}) } : res.send({message: "User already exists"});
+  //encrypt password
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(testUser.password, salt,  async function (err, hash) {
+      // change password to encrypted password and store user in database
+      testingUser.password = hash;
+      await User.create(testingUser);
+      res.send({ message: "User is successfully signed up." });
 
-    // if user does not exists insert and send back response
+      
+    });
+  });
+  console.log(testUser.password)
 
-    // if user does exist send back response
-
-    // add try catch for error
-   
-
-
-}
-
+  // Load hash from your password DB.
+  
+ 
 
 
+  //encrypt password
 
+  // refine user as json
+
+  //check if user exists by id
+  //
+
+
+
+  // if user does not exists insert and send back response
+
+  // if user does exist send back response
+
+  // add try catch for error
+};
 
 module.exports = signUpController;
