bebd29263a3f862afdd48e231f467bec33bb6d41
jack soesman <jsoesman1@yahoo.com>
2025-05-08 19:43:46 -0400

    adjust cors for cookies

--- backend/middleware.js
+++ backend/middleware.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 
 const applyMiddleware = (server) => {
-  server.use(cors());
+  server.use(cors({
+    origin: 'http://3.217.238.48', // 👈 use your frontend's origin
+    credentials: true              // 👈 allow cookies/auth headers
+  }));
   server.use(bodyParser.json());
   server.use(cookieParser());
 
