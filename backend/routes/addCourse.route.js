3c886a2c8fc25ee99923daef65a77e1631d45bd4
jack soesman <jsoesman1@yahoo.com>
2025-05-08 20:31:18 -0400

    change controller to get

--- backend/routes/addCourse.route.js
+++ backend/routes/addCourse.route.js
@@ -2,6 +2,6 @@ const addCourseRouter = require("express").Router()
 const addCourseController = require("../controllers/addCourse.controller.js")
 
 
-addCourseRouter.post("/add", addCourseController)
+addCourseRouter.get("/add", addCourseController)
 
 module.exports = addCourseRouter;
